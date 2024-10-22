ab4645acf1746e17d0228a08dacfb03b21fe79bb
Wojciech Stark <66268167+wgst@users.noreply.github.com>
2024-10-22 10:48:18 +0100

    add prints

--- src/ase_conversions.jl
+++ src/ase_conversions.jl
@@ -7,16 +7,25 @@ const uVelocity = sqrt(u"eV" / u"u")
 
 
 function ase_to_system(S::Type{<:AbstractSystem}, ase_atoms::Py)
-    box = [pyconvert(Vector, ase_atoms.cell[i])u"Å" for i = 0:2]
-
+    print(1)
+    box = tuple([pyconvert(Vector, ase_atoms.cell[i])u"Å" for i = 0:2] ...)
+    print(2)
     atnums     = pyconvert(Vector, ase_atoms.get_atomic_numbers())
+    print(3)
     atsyms     = pyconvert(Vector, ase_atoms.get_chemical_symbols())
+    print(4)
     atmasses   = pyconvert(Vector, ase_atoms.get_masses())
+    print(5)
     positions  = pyconvert(Matrix, ase_atoms.get_positions())
+    print(6)
     velocities = pyconvert(Matrix, ase_atoms.get_velocities())
+    print(7)
     magmoms    = pyconvert(Vector, ase_atoms.get_initial_magnetic_moments())
+    print(8)
     charges    = pyconvert(Vector, ase_atoms.get_initial_charges())
+    print(9)
     ase_info   = pyconvert(Dict{String,Any}, ase_atoms.info)
+    print(10)
 
     atoms = map(1:length(atnums)) do i
         AtomsBase.Atom(atnums[i], positions[i, :]u"Å", velocities[i, :] * uVelocity;
@@ -26,7 +35,7 @@ function ase_to_system(S::Type{<:AbstractSystem}, ase_atoms::Py)
                        magnetic_moment=magmoms[i],
                        charge=charges[i]u"e_au")
     end
-
+    print(11)
     # Parse extra data in info struct
     info = Dict{Symbol, Any}()
     for (k, v) in ase_info
@@ -36,8 +45,9 @@ function ase_to_system(S::Type{<:AbstractSystem}, ase_atoms::Py)
             info[Symbol(k)] = v
         end
     end
-
+    print(11)
     pbcs = [p ? true : false for p in pyconvert(Vector, ase_atoms.pbc)]
+    print(12)
     PythonCall.pyconvert_return(atomic_system(atoms, box, pbcs; info...))
 end
 
